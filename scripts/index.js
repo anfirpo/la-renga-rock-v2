af1eb705e566794cff3e9c66e255db8e7de4ad34
anfirpo <adrian.firpo581@gmail.com>
2023-10-10 19:51:25 -0300

    Arreglé el Navbar

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,25 @@
-// Esto cerrará el Navbar después de hacer clic en un enlace
-document.querySelectorAll('.navbar-nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+/* CIERRO EL NAVBAR-TOGGLER UNA VEZ ELEGIDA UNA OPCIÓN DEL NAVBAR */
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+document.addEventListener('DOMContentLoaded', function () {
+    // Obtén una referencia al botón del Navbar que activa la funcionalidad de colapso
+    var navbarToggler = document.querySelector('.navbar-toggler');
+
+    // Obtén una referencia a la barra de navegación (Navbar)
+    var navbarCollapse = document.querySelector('.navbar-collapse');
+
+    // Obtén una lista de todos los elementos de enlace (links) dentro del Navbar
+    var navLinks = document.querySelectorAll('.nav-link');
+
+    // Añade un event listener a cada enlace para cerrar el Navbar después de hacer clic
+    navLinks.forEach(function (navLink) {
+        navLink.addEventListener('click', function () {
+            // Verifica si el Navbar está colapsado o no
+            if (!navbarCollapse.classList.contains('show')) {
+                return; // Si ya está colapsado, no hagas nada
+            }
+
+            // Si el Navbar está expandido, haz clic en el botón de colapso
+            navbarToggler.click();
         });
-
-        // Cierra el Navbar
-        document.querySelector('.navbar-collapse').classList.remove('show');
     });
 });
