54db0520add58d4b70deec2f5c45136d677bac56
anfirpo <adrian.firpo581@gmail.com>
2023-10-18 10:15:42 -0300

    Creé 3 dropdown

--- scripts/compra-entradas.js
+++ scripts/compra-entradas.js
@@ -1,33 +1,61 @@
-/* Crear los Arrays en JavaScript: */
+/* Crear los Arrays en JavaScript e inicializar las variables: */
 let fechas = ["AGO 26 - BUENOS AIRES, ARGENTINA - ESTADIO TOMÁS ADOLFO DUCÓ", "SEP 12 - CÓRDOBA, ARGENTINA - ESTADIO MARIO ALBERTO KEMPES", "SEP 24 - MENDOZA, ARGENTINA - ESTADIO MALVINAS ARGENTINAS", "OCT 26 - TIERRA DEL FUEGO, ARGENTINA - HANGAR DEL VIEJO AEROPUERTO", "NOV 6 - BUENOS AIRES, ARGENTINA - ESTADIO LIBERTADORES DE AMÉRICA"];
 
-let sectores = ["PLATEA ALTA", "PLATEA BAJA", "POPULAR", "CAMPO", "CAMPO VIP"]
+let sectores = ["PLATEA ALTA - $15000", "PLATEA BAJA - $20000", "POPULAR - $10000", "CAMPO - $12500", "CAMPO VIP - $25000"]
 
-/* Llenar el Dropdown con JavaScript: */
-let dropdownFechas = document.getElementById("dropdownMenuFechas");
+let cantidad = [1, 2, 3, 4, 5]
 
+let dropdownMenuFechas = document.getElementById("dropdownMenuFechas");
+let dropdownMenuSectores = document.getElementById("dropdownMenuSectores");
+let dropdownMenuCantidad = document.getElementById("dropdownMenuCantidad");
+let dropdownSectores = document.getElementById("dropdownSectores");
+let dropdownCantidad = document.getElementById("dropdownCantidad");
+
+/* Llenar el Dropdown de fechas con JavaScript: */
 fechas.forEach(function (opcion) {
     let elemento = document.createElement("a");
     elemento.classList.add("dropdown-item");
     elemento.textContent = opcion;
-    elemento.href = "#"; // Puedes asignar un enlace aquí si es necesario
+    elemento.href = "#";
     elemento.addEventListener("click", function () {
-        valorSeleccionado = opcion;
-        console.log("Valor seleccionado:", valorSeleccionado);
-        // Aquí puedes realizar acciones con el valor seleccionado
+        fechaElegida = opcion;
+        console.log("Valor seleccionado:", fechaElegida);
+
+        if (dropdownSectores.classList.contains("show")) {
+        } else {
+            dropdownSectores.classList.toggle("show");
+        }
     });
-    dropdownFechas.appendChild(elemento);
+    dropdownMenuFechas.appendChild(elemento);
 });
 
+/* Llenar el Dropdown de sectores con JavaScript: */
 sectores.forEach(function (sector) {
     let elemento = document.createElement("a");
     elemento.classList.add("dropdown-item");
     elemento.textContent = sector;
-    elemento.href = "#"; // Puedes asignar un enlace aquí si es necesario
+    elemento.href = "#";
     elemento.addEventListener("click", function () {
-        valorSeleccionado = sector;
-        console.log("Valor seleccionado:", valorSeleccionado);
-        // Aquí puedes realizar acciones con el valor seleccionado
+        sectorElegido = sector;
+        console.log("Valor seleccionado:", sectorElegido);
+
+        if (dropdownCantidad.classList.contains("show")) {
+        } else {
+            dropdownCantidad.classList.toggle("show");
+        }
     });
-    dropdownSectores.appendChild(elemento);
+    dropdownMenuSectores.appendChild(elemento);
+});
+
+/* Llenar el Dropdown de cantidad con JavaScript: */
+cantidad.forEach(function (cant) {
+    let elemento = document.createElement("a");
+    elemento.classList.add("dropdown-item");
+    elemento.textContent = cant;
+    elemento.href = "#";
+    elemento.addEventListener("click", function () {
+        cantidadElegida = cant;
+        console.log("Valor seleccionado:", cantidadElegida);
+    });
+    dropdownMenuCantidad.appendChild(elemento);
 });
