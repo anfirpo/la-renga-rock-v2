6ab9bc17da17a1d17f9cabc21dc34f5caae3b810
anfirpo <adrian.firpo581@gmail.com>
2023-11-02 20:36:00 -0300

    Lo puse más prolijito

--- scripts/index.js
+++ scripts/index.js
@@ -5,25 +5,26 @@
 /* CIERRO EL NAVBAR-TOGGLER UNA VEZ ELEGIDA UNA OPCIÓN DEL NAVBAR */
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Obtengo una referencia al botón del Navbar que activa la funcionalidad de colapso
+    /*  Obtengo una referencia al botón del Navbar que activa la funcionalidad de colapso */
     let navbarToggler = document.querySelector(".navbar-toggler")
 
-    // Obtengo una referencia a la barra de navegación (Navbar)
+    /* Obtengo una referencia a la barra de navegación (Navbar) */
     let navbarCollapse = document.querySelector(".navbar-collapse")
 
-    // Obtengo una lista de todos los elementos de enlace (links) dentro del Navbar
+    /* Obtengo una lista de todos los elementos de enlace (links) dentro del Navbar */
     let navLinks = document.querySelectorAll(".nav-link")
 
-    // Añado un event listener a cada enlace para cerrar el Navbar después de hacer clic
+    /* Añado un event listener a cada enlace para cerrar el Navbar después de hacer click */
     navLinks.forEach(function (navLink) {
         navLink.addEventListener("click", function () {
-            // Verifica si el Navbar está colapsado o no
+            /* Verifica si el Navbar está colapsado o no */
             if (!navbarCollapse.classList.contains("show")) {
-                return; // Si ya está colapsado, no hagas nada
+                /* Si ya está colapsado, no hagas nada */
+                return
             }
 
-            // Si el Navbar está expandido, haz clic en el botón de colapso
+            /* Si el Navbar está expandido, haz clic en el botón de colapso */
             navbarToggler.click()
-        });
-    });
-});
+        })
+    })
+})
